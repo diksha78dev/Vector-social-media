cccc9266f440e9803fe0a1f19eed05e4786feeaa
Shivayan Chakravarty <shivayanchak@gmail.com>
2026-02-11 16:51:28 +0530

    google oAuth redirection url added

--- server/src/controllers/googleAuth.controller.js
+++ server/src/controllers/googleAuth.controller.js
@@ -11,16 +11,19 @@ export const googleAuthCallback = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            sameSite:
+                process.env.NODE_ENV === "production" ? "none" : "lax",
             path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
+        const FRONTEND_URL = process.env.FRONTEND_URL;
         if (!user.isProfileComplete) {
-            return res.redirect("http://localhost:3000/auth/profile?register=google");
+            return res.redirect(`${FRONTEND_URL}/auth/profile?register=google`);
         }
-        res.redirect("http://localhost:3000/main?login=google");
+        res.redirect(`${FRONTEND_URL}/main?login=google`);
     } catch (err) {
         console.error(err);
-        res.redirect("http://localhost:3000/auth/login?error=google");
+        const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+        res.redirect(`${FRONTEND_URL}/auth/login?error=google`);
     }
 };
