d4f8888e49d4fdb6f032d2aa6538eb9f4e93e93f
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-04-19 12:27:55 +0530

    fix(issue-36): Wrap /main layout with ProtectedLayout to enforce authentication

--- client/app/main/layout.tsx
+++ client/app/main/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/layouts/Sidebar";
+import ProtectedLayout from "@/components/ProtectedLayout";
 
 export default function AppLayout({
   children,
@@ -6,11 +7,13 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen  bg-[url('/vector-home-bg-light.png')]  dark:bg-[url('/vector-home-bg-dark.png')]  bg-cover bg-center bg-no-repeat">
-      <Sidebar />
-      <main className="flex-1">
-        {children}
-      </main>
-    </div>
+    <ProtectedLayout>
+      <div className="flex min-h-screen bg-[url('/vector-home-bg-light.png')] dark:bg-[url('/vector-home-bg-dark.png')] bg-cover bg-center bg-no-repeat">
+        <Sidebar />
+        <main className="flex-1">
+          {children}
+        </main>
+      </div>
+    </ProtectedLayout>
   );
 }
