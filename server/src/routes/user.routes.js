6939f78692bf0f429bc5694d2412174c92f7b883
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-04-28 18:49:20 +0530

    feat: add /suggestions route with authentication

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
-import { getAllUsers, getFollowers, getFollowing, getUserProfile, searchUsers, toggleFollowUser, updateProfile, uploadAvatar } from "../controllers/user.controller.js";
+import { getAllUsers, getFollowers, getFollowing, getUserProfile, searchUsers, toggleFollowUser, updateProfile, uploadAvatar, getSuggestedUsers } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/avatar", authMiddleware, upload.single("avatar"), uploadAvatar);
 userRouter.put("/update-profile", authMiddleware, updateProfile);
 userRouter.put("/:id/follow", authMiddleware, toggleFollowUser);
+userRouter.get("/suggestions", authMiddleware, getSuggestedUsers);
 userRouter.get("/all", getAllUsers);
 userRouter.get("/search", searchUsers);
 userRouter.get("/:username", getUserProfile);
